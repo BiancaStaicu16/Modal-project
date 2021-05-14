00ebdf4d1d972293672e1ddc5cd2e4832e183a0f
Bianca Staicu <biancastaicu_bella@yahoo.com>
2021-05-14 09:59:56 +0100

    New component added.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
   <h1>{{ title }}</h1>
+  <Modal />
+  <br>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click me</button>
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
+  components: {Modal },
   data() {
     return{
       title: 'My First Vue App :)'
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
     }
   }
 }
@@ -24,7 +37,8 @@ export default {
   margin-top: 60px;
 }
 h1 {
+  border-bottom: 1px solid #ddd;
   display: inline-block;
-  padding-bottom: 100px ;
+  padding-bottom: 10px ;
 }
 </style>
