4befa568154a65d10eab0cab347acef59f1e1255
Bianca Staicu <biancastaicu_bella@yahoo.com>
2021-05-23 17:27:09 +0100

    New modal added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>{{ title }}</h1>
-  <div v-if="showModal">
+  <teleport to=".modals" v-if="showModal">
     <Modal theme="" @close="toogleModal">
       <template v-slot:links>
         <a href="#">Get your package now</a>
@@ -10,13 +10,22 @@
       <p>Each module is explained carefully by our best!</p>
       
     </Modal>
-  </div>
+  </teleport>
+
+   <teleport to=".modals" v-if="showModal2">
+    <Modal @close="toogleModal2">
+      <h1>Sign up for the newsletter!</h1>
+      <p>For updates and more info!</p>
+    </Modal>
+  </teleport>
+
   <br>
   <input type="text" ref="name">
   <button @click="handleClick">Click me</button>
   <br>
   <p>Welcome...</p>
   <button @click="toogleModal">Open Modal</button>
+  <button @click="toogleModal2">Open Modal</button>
 </template>
 
 <script>
@@ -28,9 +37,8 @@ export default {
   data() {
     return{
       title: 'My First Vue App :)',
-      header: 'Sign up for learning more!',
-      text: 'Decide on something to learn and get started!',
-      showModal: false
+      showModal: false,
+      showModal2: false
     }
   },
   methods: {
@@ -41,13 +49,16 @@ export default {
     },
     toogleModal() {
       this.showModal = !this.showModal
+    },
+    toogleModal2() {
+      this.showModal2 = !this.showModal2
     }
   }
 }
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
